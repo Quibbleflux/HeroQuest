a7ed13767b93a18c4b75a27818ea8599f482e1bb
Quibbleflux <mjwallace1989@gmail.com>
2016-08-03 15:23:51 -0230

    Added more base classes

---
+++ HeroQuest/src/main/Game.java
@@ -0,0 +1,14 @@
+
+package main;
+
+/**
+ *
+ * @author Matts
+ */
+public class Game {
+    private Party party;
+    private Inventory inventory;
+    public Game() {
+        
+    }    
+}
---
+++ HeroQuest/src/main/Party.java
@@ -0,0 +1,11 @@
+package main;
+
+/**
+ *
+ * @author Matts
+ */
+public class Party {
+    public Party() {
+        
+    }
+}
