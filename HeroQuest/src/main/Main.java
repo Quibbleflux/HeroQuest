8fee22d9a78b1183584041a683ccaa33abde4fdd
Matthew Greenslade <mattgr2010@gmail.com>
2016-08-03 14:06:14 -0230

    Added version, removed header

--- HeroQuest/src/main/Main.java
+++ HeroQuest/src/main/Main.java
@@ -1,13 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package main;
 
 /**
  *
  * @author Matthew
+ * @version Alpha
  */
 public class Main {
 
@@ -17,5 +13,4 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Yes");
     }
-    
 }
